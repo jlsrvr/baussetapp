7442f6abf106f3b37bca82173e2d0df2d3431b27
Jules Rivoire <julesriv@gmail.com>
2019-07-17 18:28:49 +0200

    created location migration

---
+++ db/migrate/20190717162720_create_locations.rb
@@ -0,0 +1,11 @@
+class CreateLocations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :locations do |t|
+      t.reference :user
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
