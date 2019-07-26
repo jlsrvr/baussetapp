76f1ae1201915599aaa88bfb171970b3fbfc8c3e
Jules Rivoire <julesriv@gmail.com>
2019-07-26 15:55:13 +0200

    changes to typos in validatiosn afetr testing in the rails development environment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :locations
   has_many :bookings_as_host, through: :locations
   has_many :bookings
   has_many :beds, through: :locations
