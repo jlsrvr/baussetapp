8511662385ec6bc1b45e0e60415aa312fff3b075
Jules Rivoire <julesriv@gmail.com>
2019-07-26 15:15:46 +0200

    attempt at doing associations for the models

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -1,2 +1,4 @@
 class Bed < ApplicationRecord
+  belongs_to :locations
+  has_many :bookings
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,5 @@
 class Location < ApplicationRecord
+  has_many :beds, dependent: :destroy
+  has_many :bookings, through: :beds, dependent: :destroy
+  belongs_to :users
 end
