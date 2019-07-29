4b706b7e22e70f6649bd30019597dc598402d2b2
Jules Rivoire <julesriv@gmail.com>
2019-07-29 18:38:39 +0200

    simple form for creating a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,24 @@ class BookingsController < ApplicationController
     @bookings = policy_scope(Booking)
   end
 
+  def new
+    # @beds = Bed.all
+    @booking = Booking.new
+    authorize @booking
+  end
+
+  def create
+    @booking = current_user.booking.build(booking_params)
+    # we need `restaurant_id` to asssociate booking with corresponding restaurant
+    @booking.save
+  end
+
   def show
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:content)
   end
 end
