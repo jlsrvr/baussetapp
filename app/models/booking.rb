2dadc9623fd5c6d86e39e829ccd7437ad45bda76
Jules Rivoire <julesriv@gmail.com>
2019-07-30 15:05:23 +0200

    addded status to a booking and fixed bookings as host method

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :bed
+
+  validates :status, inclusion: { in: %w[pending accepted declined] }
+  # validates :date, presence: true
 end
