fecf0cb00c09229e1141f50c39ebb12768db628a
Jules Rivoire <julesriv@gmail.com>
2019-07-30 17:37:40 +0200

    validations bed et un peu booking et ajout de default values pour status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :bed
 
   validates :status, inclusion: { in: %w[pending accepted declined] }
-  # validates :date, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :bed, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_085319) do
+ActiveRecord::Schema.define(version: 2019_07_30_134356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_085319) do
     t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "pending"
     t.index ["bed_id"], name: "index_bookings_on_bed_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
