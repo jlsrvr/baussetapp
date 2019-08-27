0e08a4e5ac693b541d1c2000bf2b1479c45e5023
Jules Rivoire <julesriv@gmail.com>
2019-08-27 11:59:29 +0200

    removed bed from bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_134356) do
+ActiveRecord::Schema.define(version: 2019_08_27_095418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,14 +26,12 @@ ActiveRecord::Schema.define(version: 2019_07_30_134356) do
 
   create_table "bookings", force: :cascade do |t|
     t.bigint "user_id"
-    t.bigint "bed_id"
     t.text "message"
     t.date "start_date"
     t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: "pending"
-    t.index ["bed_id"], name: "index_bookings_on_bed_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
@@ -62,7 +60,6 @@ ActiveRecord::Schema.define(version: 2019_07_30_134356) do
   end
 
   add_foreign_key "beds", "locations"
-  add_foreign_key "bookings", "beds"
   add_foreign_key "bookings", "users"
   add_foreign_key "locations", "users"
 end
