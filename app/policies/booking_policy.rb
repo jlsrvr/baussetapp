730d58123d5012ce1320f5bdd955bc7570963873
Jules Rivoire <julesriv@gmail.com>
2019-08-30 12:27:45 +0200

    removed useless show? policy from booking policy

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -9,14 +9,6 @@ class BookingPolicy < ApplicationPolicy
     true
   end
 
-  def show?
-    user_is_owner_or_admin
-    Booking.where(id: record.id).exists?
-    # scope.where(id: record.id).exists?
-    # scope sert dans le application policy pour designer le model dans le quel on est
-    # record
-  end
-
   private
 
   def user_is_owner_or_admin
